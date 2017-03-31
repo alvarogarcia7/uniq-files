61b43ccc346823e8a804bb01f76a068ebfcdaef0
Alvaro Garcia <alvaro.garcia@codurance.com>
2017-03-31 20:38:14 +0200

    Save WIP for publishing multiple messages

---
+++ test/uniq_files/core_test.clj
@@ -0,0 +1,35 @@
+(ns uniq-files.core-test
+  (:require [langohr.core :as rmq]
+            [langohr.channel :as lch]
+            [langohr.queue :as lq]
+            [langohr.consumers :as lc]
+            [langohr.basic :as lb]
+            [uniq-files.core :refer :all]))
+
+(def ^{:const true}
+default-exchange-name "")
+
+(defn message-handler
+  [ch {:keys [content-type delivery-tag type] :as meta} ^bytes payload]
+  (println (format "[consumer] Received a message: %s, delivery tag: %d, content type: %s, type: %s"
+                   (String. payload "UTF-8") delivery-tag content-type type)))
+
+(defn main
+  [& args]
+  (println rmq/*default-config*)
+  (let [_ (println "BP0")
+        conn (rmq/connect)
+        _ (println "BP1")
+        ch (lch/open conn)
+        qname "langohr.examples.hello-world"]
+
+    (println (format "[main] Connected. Channel id: %d" (.getChannelNumber ch)))
+    (lq/declare ch qname {:exclusive false :auto-delete true})
+    (lc/subscribe ch qname message-handler {:auto-ack true})
+    (doall
+      (for [i (range 10)]
+             (lb/publish ch default-exchange-name qname (str "Hello!" i) {:content-type "text/plain" :type "greetings.hi"})))
+    (Thread/sleep 2000)
+    (println "[main] Disconnecting...")
+    (rmq/close ch)
+    (rmq/close conn)))
