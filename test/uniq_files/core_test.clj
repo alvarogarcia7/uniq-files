e7e7d313c037ef2b13d238d8fae74f2d8306e456
Alvaro Garcia <derecerca@gmail.com>
2017-04-03 09:35:42 +0200

    Remove code related to messaging

--- test/uniq_files/core_test.clj
+++ test/uniq_files/core_test.clj
@@ -1,20 +1,5 @@
 (ns uniq-files.core-test
-  (:require [langohr.core :as rmq]
-            [langohr.channel :as lch]
-            [langohr.queue :as lq]
-            [langohr.consumers :as lc]
-            [langohr.basic :as lb]
-            [uniq-files.core :refer :all]
-            [clojure.string :require :split]))
-
-(def ^{:const true}
-default-exchange-name "")
-
-(defn message-handler
-  [ch {:keys [content-type delivery-tag type] :as meta} ^bytes payload]
-  (println (format "[consumer] Received a message: %s, delivery tag: %d, content type: %s, type: %s"
-                   (String. payload "UTF-8") delivery-tag content-type type)))
-
+  (:require [clojure.string :require :split]))
 (defn
   group-by-hash
   [lines]
@@ -45,30 +30,6 @@ default-exchange-name "")
   (with-open [rdr (reader path)]
     (doall (line-seq rdr))))
 
-(defn
-  connect-to-mq
-  []
-  (let [conn (rmq/connect)
-        ch (lch/open conn)]
-    {:connection conn
-     :channel    ch}))
-
-(defn
-  disconnect-from-mq
-  [mq]
-  (let [{conn :connection ch :channel} mq]
-    (rmq/close ch)
-    (rmq/close conn)))
-
-(defn configure-handler
-  [ch qname message-handler]
-  (lq/declare ch qname {:exclusive false :auto-delete true})
-  (lc/subscribe ch qname message-handler {:auto-ack true}))
-
-(defn
-  get-last
-  [_ ^bytes payload]
-  (println (format "[get-last] Received a message: %s" (String. payload "UTF-8"))))
 
 
 (defn
