ab64a50f537daab88161af1e27e30b87d9856490
Alvaro Garcia <derecerca@gmail.com>
2017-04-05 17:48:23 +0200

    Green: test them in any order

--- test/uniq_files/core_test.clj
+++ test/uniq_files/core_test.clj
@@ -22,9 +22,11 @@
       (map (fn
              [[hash filenames]]
              (let [filenames (sort filenames)]
-               (println (str "#HASH = " hash))
-               (println (str "# keep " (last filenames)))
-               (doall (map #(println (str "rm " %)) (butlast filenames)))))))))
+               ;(println (str "#HASH = " hash))
+               (concat
+                 (list (str "# keep " (last filenames)))
+                 (doall (map #(str "rm " %) (butlast filenames)))))))
+      flatten)))
 
 
 (facts
@@ -33,14 +35,22 @@
     "happy path"
     (lines "resources/files/lines.txt") => ["1" "a" "b"]))
 
+(defn-
+  same-in-any-order
+  [actual expected]
+  (clojure.set/difference (into #{} actual) (into #{} expected)) => #{})
+
 (facts
   "grouping by contents"
   (fact
     "acceptance test"
-    (group-by-hash (lines "resources/example-1/md5.txt")) => '(
-                                                                "rm 2016-1.txt" ;60b725f10c9c85c70d97880dfe8191b3
-                                                                "rm 2016-2.txt" ;60b725f10c9c85c70d97880dfe8191b3
-                                                                "# keep 2016-3.txt" ;e29311f6f1bf1af907f9ef9f44b8328b
-                                                                "# keep 2017-1.txt" ;60b725f10c9c85c70d97880dfe8191b3
-                                                                "# keep 2017-2.txt" ;bfcc9da4f2e1d313c63cd0a4ee7604e9
-                                                                )))
+    (let [actual (group-by-hash (lines "resources/example-1/md5.txt"))
+          expected (list
+             "rm 2016-1.txt"                                ;60b725f10c9c85c70d97880dfe8191b3
+             "rm 2016-2.txt"                                ;60b725f10c9c85c70d97880dfe8191b3
+             "# keep 2016-3.txt"                            ;e29311f6f1bf1af907f9ef9f44b8328b
+             "# keep 2017-1.txt"                            ;60b725f10c9c85c70d97880dfe8191b3
+             "# keep 2017-2.txt"                            ;bfcc9da4f2e1d313c63cd0a4ee7604e9
+             )]
+      (same-in-any-order actual expected)
+      )))
