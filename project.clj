af76dabda9364d0f3788c265b0d130edda457801
Alvaro Garcia <alvaro.garcia@codurance.com>
2017-04-05 10:24:41 +0200

    Add test dependencies

--- project.clj
+++ project.clj
@@ -6,4 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot uniq-files.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev     {:dependencies [[midje "1.6.3"]]
+                       :plugins [[lein-midje "3.1.3"]
+                                 [com.jakemccrary/lein-test-refresh "0.6.0"]]}})
